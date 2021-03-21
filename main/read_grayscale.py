5bf9259aee76b9cfaaab5e4782f2d8dbffb0fd38
KadirTaban <kadirtaban08@gmail.com>
2021-03-21 10:02:58 +0300

    lookwebcam

--- main/read_grayscale.py
+++ main/read_grayscale.py
@@ -3,6 +3,11 @@ import cv2
 path="Resources/lena.png"
 img=cv2.imread(path)
 imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur=cv2.GaussianBlur(imgGray,(7,7),0)
+imgCanny=cv2.Canny(imgBlur,100,200)
+
 cv2.imshow("lena",img)
 cv2.imshow("Grayscale",imgGray)
+cv2.imshow("imgBlur",imgBlur)
+cv2.imshow("img canny",imgCanny)
 cv2.waitKey(0)
