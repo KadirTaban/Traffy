ed9967a0acc89e192407bb417959d21814d61f24
KadirTaban <kadirtaban08@gmail.com>
2021-03-24 00:27:51 +0300

    Caykaranın Dagları

--- main/colorDetection.py
+++ main/colorDetection.py
@@ -13,10 +13,28 @@ def empty(a):
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV",640,480)
 cv2.createTrackbar("Hue Min","HSV",0,179,empty)
+cv2.createTrackbar("Hue Max","HSV",179,179,empty)
+cv2.createTrackbar("SAT Min","HSV",0,255,empty)
+cv2.createTrackbar("SAT Max","HSV",255,255,empty)
+cv2.createTrackbar("Value Min","HSV",0,255,empty)
+cv2.createTrackbar("Value Max","HSV",255,255,empty)
 
 while True:
     _, img=cap.read()
     imgHsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    cv2.namedWindow("HSV")
+    cv2.resizeWindow("HSV", 640, 480)
+    cv2.createTrackbar("Hue Min", "HSV", 0, 179, empty)
+    cv2.createTrackbar("Hue Max", "HSV", 179, 179, empty)
+    cv2.createTrackbar("SAT Min", "HSV", 0, 255, empty)
+    cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
+    cv2.createTrackbar("Value Min", "HSV", 0, 255, empty)
+    cv2.createTrackbar("Value Max", "HSV", 255, 255, empty)
+    print(h_min)
+
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask=cv2.inRange(imgHsv,lower,upper)
 
     cv2.imshow('Original',img)
     cv2.imshow('HSC Color Space',imgHsv)
