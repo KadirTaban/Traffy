c61ccaedf2e8da342f68dd6ccef7a1fc94e2ff68
KadirTaban <kadirtaban08@gmail.com>
2021-03-23 16:42:29 +0300

    a

--- main/warpPerspective.py
+++ main/warpPerspective.py
@@ -7,7 +7,7 @@ width, height= 450,550
 
 pts1 = np.float32([[134,194],[243,224],[129,339],[245,355]])
 pts2= np.float32([[0,0],[450,0],[0,550],[450,550]])
-matrix= cv2.getPerspectiveTransform(pts1,pts2)
+matrix= cv2.getPerspectiveTransform(pts1,pts2)#ex:https://pysource.com/2018/02/14/perspective-transformation-opencv-3-4-with-python-3-tutorial-13/
 imgOutput=cv2.warpPerspective(img,matrix,(width,height))
 
 
