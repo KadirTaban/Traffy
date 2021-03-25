87acd8dce1cfae677f1513dbe6845fef468c4933
KadirTaban <kadirtaban08@gmail.com>
2021-03-25 22:42:06 +0300

    learncommit

--- main/contoursDetection.py
+++ main/contoursDetection.py
@@ -7,11 +7,15 @@ frameHeight = 480
 cap= cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
+def empty(a):
+    pass
 
 cv2.namedWindow("Parameters")
-cv2.resize("Parameters",640,240)
-cv2.createTrackbar("Threshold1","Parameters",150,255,empty)
-cv2.createTrackbar("Threshold2","Parameters",255,255,empty)
+cv2.resizeWindow("Parameters",640,240)
+cv2.createTrackbar("Threshold1","Parameters",23,255,empty)
+cv2.createTrackbar("Threshold2","Parameters",20,255,empty)
+
+
 def stackImages(scale, imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -43,16 +47,37 @@ def stackImages(scale, imgArray):
         ver = hor
     return ver
 
+def getContours(img,imgContour):
+    contours, hierarchy= cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(imgContour,contours,-1,(255,0,255),7)
 
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area> 1000:
+
+            cv2.drawContours(imgContour, contours, -1, (255, 0, 255), 7)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri, True)
+            print(approx)
 
 while True:
     success, img = cap.read()
+    imgContour=img.copy()
     imgBlur=cv2.GaussianBlur(img, (7,7), 1)
     imgGray=cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-    imgStack=stackImages(0.8,([img,imgBlur,imgGray]))
+
+    threshold1= cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2=cv2.getTrackbarPos("Threshold2","Parameters")
+    imgCanny =cv2.Canny(imgGray,threshold1,threshold2)
+
+    kernel=np.ones((5,5))
+    imgDil=cv2.dilate(imgCanny, kernel, iterations=1)
+    getContours(imgDil,imgContour)
+    imgStack=stackImages(0.8,([img,imgGray,imgCanny],
+                                [imgDil,imgContour,imgContour]))
 
 
     cv2.imshow("Result",imgStack)
 
-    if cv2.waitKey(1) & 0xFF== ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
