4d18b4ddb15809505499cbcd2baa63ce86503d5c
KadirTaban <kadirtaban08@gmail.com>
2021-03-27 11:29:49 +0300

    haarcascade

--- main/contoursDetection.py
+++ main/contoursDetection.py
@@ -25,8 +25,7 @@ def stackImages(scale, imgArray):
     if rowsAvailable:
         for x in range(0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
                 else:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),None, scale, scale)
                 if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
