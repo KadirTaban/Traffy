7d15e2141dc0c55a344985d2f086979adc9460b6
KadirTaban <kadirtaban08@gmail.com>
2021-04-08 11:58:06 +0300

    trafficsigntest

--- Traffic-Signs/TrafficSign_Test.py
+++ Traffic-Signs/TrafficSign_Test.py
@@ -30,4 +30,33 @@ def preprocessing(img):
     img=img/255
     return img
 
-def getClassname=
+def getClassname(classNo):
+    if classNo==0:return"Speed limit(80km/h)"
+    elif classNo==1:return "No Passing"
+    elif classNo == 2:return "Stop"
+    elif classNo== 3:return "Yield"
+
+
+while True:
+    #Read Image
+    success, imgOriginal=cap.read()
+    #Process Image
+    img=np.asarray(imgOriginal)
+    img=cv2.resize(img,(32,32))
+    img=preprocessing(img)
+    cv2.imshow("Processed Image",img)
+    img=img.reshape(1,32,32,1)
+
+    cv2.putText(imgOriginal,"CLASS",(20,35),font,0.75,(0,0,255),2,cv2.LINE_AA)
+    cv2.putText(imgOriginal,"Probability",(20,75),font,0.75,(0,0,255),2,cv2.LINE_AA)
+    #PREDICT IMAGE
+    predictions=model.predict(img)
+    classIndex=model.predict_classes(img)
+    probabilityValue=np.amax(predictions)
+    if probabilityValue > threshOld:
+        #print(getClassName(classIndex))
+        cv2.putText(imgOriginal,str(classIndex)+""+str(getClassname(classIndex)),(120,35),font,0.75,(0,0,255),2,cv2.LINE_AA)
+        cv2.putText(imgOriginal,str(round(probabilityValue*100,2))+"%",(100,75),font,0.75,(0,0,255),2,cv2.LINE_AA)
+    cv2.imshow("Result",imgOriginal)
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        break
