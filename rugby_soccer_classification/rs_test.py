c85f87e23700c6d327defb454b871c37cf4d7735
KadirTaban <kadirtaban08@gmail.com>
2021-04-17 00:13:19 +0300

    evaluate.

--- rugby_soccer_classification/rs_test.py
+++ rugby_soccer_classification/rs_test.py
@@ -2,6 +2,7 @@ from typing import Union
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import tensorflow.python.keras.losses
 from matplotlib.pyplot import plot
 from tensorflow import keras
 
@@ -84,4 +85,60 @@ x_train = np.array(x_train)/255
 x_val = np.array(x_val) / 255
 
 x_train.reshape(-1, img_size,img_size,1)
-y_train
+y_train= np.array(y_train)
+
+x_val.reshape(-1 , img_size,img_size,1)
+y_val=np.array(y_val)
+#Data augmentation on the train data
+
+datagen= ImageDataGenerator(featurewise_center=False,#set input mean to 0 over the dataset
+                            samplewise_center=False, # set each sample mean to 0
+                            featurewise_std_normalization=False, #divide each input by its std
+                            zca_whitening=False, #apply ZCA whitening
+                            rotation_range=30, #randomly zoom image
+                            width_shift_range=0.1, #randomly shift images horizontally(fraction of total width)
+                            height_shift_range=0.1, #randomly shift images vertically(fraction of total height)
+                            horizontal_flip=True, #randomly flip images
+                            vertical_flip= False) #randomly flip images
+
+
+datagen.fit(x_train)
+#Let's define a simple CNN model with 3 Convolutional layers followed by max-pooling layers.
+
+model=Sequential()
+model.add(Conv2D(32,3,padding="same",activation="relu", input_shape=(224,224,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32,3,padding="same",activation="relu"))
+model.add(MaxPooling2D())
+model.add(MaxPooling2D())
+model.add(Dropout(0.4))
+model.add(Flatten())
+model.add(Dense(128,activation="relu"))
+model.add(Dense(2,activation="softmax"))
+model.summary()
+#Let's compile the model using Adam as our optimizer and SparseCategoricalCrossentropy as the loss function
+opt= Adam(lr=0.000001)
+model.compile(optimizer=opt,loss=tensorflow.python.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+#Let's train our model for 500 epochs since our learning rate is very small
+history=model.fit(x_train,y_train,epochs=500,validation_data=(x_val,y_val))
+#Evaluationg the result
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(500)
+
+plt.figure(figsize=(15, 15))
+plt.subplot(2, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
